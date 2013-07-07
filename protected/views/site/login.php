ef5229e383dcadad6794550a0119416d66518330
Alex Barcelo <abarcelo@ac.upc.edu>
2013-07-07 08:16:27 +0200

    better login form

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -14,12 +14,12 @@ $this->pageTitle=Yii::app()->name . ' - Entrada';
 <fieldset>
   <legend>Introdueixi la següent informació per entrar al sistema</legend>
   <label>Identificador d'usuari</label>
-  <input type="text" id="LoginForm_username" value="<?php echo $model->username ?>" name="LoginForm[username]"/>
+  <input type="text" id="LoginForm_username" name="LoginForm[username]">
 
   <label>Contrasenya</label>
   <input type="password" id="LoginForm_password" name="LoginForm[password]">
-
-  <button type="submit" class="btn">Entrar</button>
+  <label></label>
+  <button type="submit" class="btn btn-large btn-primary">Entrar</button>
 
 </fieldset>
 </form><!-- form -->
